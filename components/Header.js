c41615c38ba44053c5c460b9c4dd4b4d30eb270e
mojafa <jmohsa@gmail.com>
2021-06-18 15:05:03 +0300

    new change

---
+++ components/Header.js
@@ -0,0 +1,33 @@
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from '@heroicons/react/outline';
+import Image from 'next/image';
+import HeaderItem from './HeaderItem';
+function Header() {
+  return (
+    <header className="">
+      {/* <h1>This is our header</h1> */}
+      <div>
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
+        <HeaderItem title="COLLECTION" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+      />
+    </header>
+  );
+}
+
+export default Header;
